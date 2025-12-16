ed4e8535a697c6ede2bb652c4430d24ebb80b482
Ayala4159 <ayala32952@gmail.com>
2025-12-16 15:37:18 +0200

    complete the models

--- App/models.py
+++ App/models.py
@@ -1,14 +1,53 @@
-from django.db import models
+import datetime
 
-class team(models.Model):
-    team_name=models.CharField()
+from django.db import models
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.team_name
+
 
 class User(models.Model):
-    first_name = models.CharField(max_length=100,),
-    last_name = models.CharField(max_length=100),
-    email = models.EmailField(),
-    role=models.CharField(max_length=100,choices=[('Manager','Manager'),('Team_Member','Team_Member')])
-    team=models.ForeignKey(team)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password=models.CharField(max_length=100)
+    role = models.CharField(max_length=100, choices=[('Manager', 'Manager'), ('Team_Member', 'Team_Member')], null=True)
+    team = models.ForeignKey("Team", on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
-class task(models.Model):
+def start_date_validation(value):
+    if value < timezone.now().date():
+        raise ValidationError("The date cannot be in the past.")
+
+
+def end_date_validation(value):
+    if value <= timezone.now().date():
+        raise ValidationError("Please enter a future date")
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField(validators=[start_date_validation])
+    end_date = models.DateField(validators=[end_date_validation])
+    status = models.CharField(max_length=100, choices=[('NEW', 'NEW'),
+                                       ('IN_PROGRESS', 'IN_PROGRESS'), ('DONE', 'DONE')], null=True, default='NEW')
+    team = models.ForeignKey("Team", on_delete=models.CASCADE)
+    owner = models.ForeignKey("User", on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} : {self.status}"
+
+    def clean(self):
+        if self.start_date and self.end_date:
+            if self.start_date > self.end_date:
+                raise ValidationError("End date cannot be earlier than start date.")
--- App/urls.py
+++ App/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path,include
+from django.urls import path, include
 from App import views
 
 urlpatterns = [
-    path("/", views.wellcome),
+    path("", views.wellcome),
     path("SignIn/", views.sign_in),
     path("SignUp/", views.sign_up)
 ]
